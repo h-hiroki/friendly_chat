123cdcfeb9beea451b6b001e060f33bbe9fde395
hhiroki <gyouzakuuka@gmail.com>
2019-12-31 20:26:25 +0900

    add text input form

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,35 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Friendlychat'),),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textEditingController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textEditingController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(
+          hintText: 'Send a message'
+        ),
+      ),
     );
   }
 }
